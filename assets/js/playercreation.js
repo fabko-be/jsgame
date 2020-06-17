8a30b6103f01628be4776bcabc0ebfb67b3e9644
Fabian Quoidbach <fabianquoidbach@gmail.com>
2020-06-17 16:50:59 +0200

    Start algo

--- assets/js/playercreation.js
+++ assets/js/playercreation.js
@@ -1,6 +1,7 @@
     const races = [
         {
-    name: "human",
+    name: "",
+    race : "human",
     maxHealth: 100,
     startHealth: 100,
     currentHealth: 100,
@@ -9,9 +10,15 @@
     damageTaken: 0.8,
     reflectChance: 0,
     lifeSteal: 0,
+    item: "",
+    dodgeChance: 0,
+    bonusHeal: 0,
+    bonusDamage: 0,
+    attackTwiceChance: 0,
 },
         {
-    name: "orc",
+    name:"",
+    race: "orc",
     maxHealth: 140,
     startHealth: 140,
     currentHeal: 140,
@@ -20,9 +27,15 @@
     damageTaken: 1,
     reflectChance: 0,
     lifeSteal: 0,
+    item:"",
+    dodgeChance: 0,
+    bonusHeal: 0,
+    bonusDamage: 0,
+    attackTwiceChance: 0,
 },
         {
-    name : "elf",
+    name :"",
+    race : "elf",
     maxHealth : 100,
 	startHealth : 100,
     currentHeal : 100,
@@ -30,10 +43,16 @@
 	maxHealing : 30,
 	damageTaken : 1,
 	reflectChance : 0.3,
-	lifeSteal : 0,
+    lifeSteal : 0,
+    item:"",
+    dodgeChance: 0,
+    bonusHeal: 0,
+    bonusDamage: 0,
+    attackTwiceChance: 0,
 },
 {
-    name : "vamp",
+    name : "",
+    race : "vampire",
     maxHealth : 100,
 	startHealth : 100,
     currentHeal : 100,
@@ -41,33 +60,21 @@
 	maxHealing : 30,
 	damageTaken : 1,
 	reflectChance : 0,
-	lifeSteal : 0.1,
+    lifeSteal : 0.1,
+    item:"",
+    dodgeChance: 0,
+    bonusHeal: 0,
+    bonusDamage: 0,
+    attackTwiceChance: 0,
 }
 ]
 
 p1 = [];
 p2 = [];
-itemp1 = [{
-    dodgeChance: 0,
-    bonusHeal: 0,
-    bonusDamage: 0,
-    attackTwiceChance: 0,
-}];
-itemp2 = [{
-    dodgeChance: 0,
-    bonusHeal: 0,
-    bonusDamage: 0,
-    attackTwiceChance: 0,
-}];
-let raceChosenp1;
-let itemChosenp1;
-let raceChosenp2;
-let itemChosenp2;
 
 // Début de la création du P1
 document.getElementById("rhum1").addEventListener("click", () => {
     p1[0] = races[0] 
-    raceChosenp1 = "Humain"
     document.getElementById("rhum1").style.transform = "scale(1.5)"
     document.getElementById("rorc1").style.transform = "scale(1)"
     document.getElementById("relf1").style.transform = "scale(1)"
@@ -76,7 +83,6 @@ document.getElementById("rhum1").addEventListener("click", () => {
 });
 document.getElementById("rorc1").addEventListener("click", () => {
     p1[0] = races[1] 
-    raceChosenp1 = "Orc"
     document.getElementById("rorc1").style.transform = "scale(1.5)"
     document.getElementById("rhum1").style.transform = "scale(1)"
     document.getElementById("relf1").style.transform = "scale(1)"
@@ -84,7 +90,6 @@ document.getElementById("rorc1").addEventListener("click", () => {
 });
 document.getElementById("relf1").addEventListener("click", () => {
     p1[0] = races[2]
-    raceChosenp1 = "Elf"
     document.getElementById("relf1").style.transform = "scale(1.5)"
     document.getElementById("rhum1").style.transform = "scale(1)"
     document.getElementById("rorc1").style.transform = "scale(1)"
@@ -92,7 +97,6 @@ document.getElementById("relf1").addEventListener("click", () => {
 });
 document.getElementById("rvamp1").addEventListener("click", () => {
     p1[0] = races[3] 
-    raceChosenp1 = "Vampire"
     document.getElementById("rvamp1").style.transform = "scale(1.5)"
     document.getElementById("rhum1").style.transform = "scale(1)"
     document.getElementById("rorc1").style.transform = "scale(1)"
@@ -100,44 +104,28 @@ document.getElementById("rvamp1").addEventListener("click", () => {
 });
 
 document.getElementById("bootsp1").addEventListener("click", () => {
-    itemp1.dodgeChance = 0.3
-    itemp1.bonusHeal = 0
-    itemp1.bonusDamage = 0
-    itemp1.attackTwiceChance = 0
-    itemChosenp1 = "boots";
+    p1[0].item = "boots";
     document.getElementById("bootsp1").style.transform = "scale(1.5)"
     document.getElementById("staffp1").style.transform = "scale(1)"
     document.getElementById("swordp1").style.transform = "scale(1)"
     document.getElementById("bowp1").style.transform = "scale(1)"
 });
 document.getElementById("staffp1").addEventListener("click", () => {
-    itemp1.dodgeChance = 0
-    itemp1.bonusHeal = 0.2
-    itemp1.bonusDamage = 0
-    itemp1.attackTwiceChance = 0
-    itemChosenp1 = "Staff";
+    p1[0].item = "staff";
     document.getElementById("bootsp1").style.transform = "scale(1)"
     document.getElementById("staffp1").style.transform = "scale(1.5)"
     document.getElementById("swordp1").style.transform = "scale(1)"
     document.getElementById("bowp1").style.transform = "scale(1)"
 });
 document.getElementById("swordp1").addEventListener("click", () => {
-    itemp1.dodgeChance = 0
-    itemp1.bonusHeal = 0
-    itemp1.bonusDamage = 0.3
-    itemp1.attackTwiceChance = 0
-    itemChosenp1 = "sword";
+    p1[0].item = "sword";
     document.getElementById("bootsp1").style.transform = "scale(1)"
     document.getElementById("staffp1").style.transform = "scale(1)"
     document.getElementById("swordp1").style.transform = "scale(1.5)"
     document.getElementById("bowp1").style.transform = "scale(1)"
 });
 document.getElementById("bowp1").addEventListener("click", () => {
-    itemp1.dodgeChance = 0
-    itemp1.bonusHeal = 0
-    itemp1.bonusDamage = 0
-    itemp1.attackTwiceChance = 0.3
-    itemChosenp1 = "bow";
+    p1[0].item = "bow";
     document.getElementById("bootsp1").style.transform = "scale(1)"
     document.getElementById("staffp1").style.transform = "scale(1)"
     document.getElementById("swordp1").style.transform = "scale(1)"
@@ -149,7 +137,6 @@ document.getElementById("bowp1").addEventListener("click", () => {
 
 document.getElementById("rhum2").addEventListener("click", () => {
     p2[0] = races[0] 
-    raceChosenp2 = "Humain"
     document.getElementById("rhum2").style.transform = "scale(1.5)"
     document.getElementById("rorc2").style.transform = "scale(1)"
     document.getElementById("relf2").style.transform = "scale(1)"
@@ -157,7 +144,6 @@ document.getElementById("rhum2").addEventListener("click", () => {
 });
 document.getElementById("rorc2").addEventListener("click", () => {
     p2[0] = races[1] 
-    raceChosenp2 = "Orc"
     document.getElementById("rhum2").style.transform = "scale(1)"
     document.getElementById("rorc2").style.transform = "scale(1.5)"
     document.getElementById("relf2").style.transform = "scale(1)"
@@ -165,7 +151,6 @@ document.getElementById("rorc2").addEventListener("click", () => {
 });
 document.getElementById("relf2").addEventListener("click", () => {
     p2[0] = races[2]
-    raceChosenp2 = "Elf"
     document.getElementById("rhum2").style.transform = "scale(1)"
     document.getElementById("rorc2").style.transform = "scale(1)"
     document.getElementById("relf2").style.transform = "scale(1.5)"
@@ -173,7 +158,6 @@ document.getElementById("relf2").addEventListener("click", () => {
 });
 document.getElementById("rvamp2").addEventListener("click", () => {
     p2[0] = races[3] 
-    raceChosenp2 = "Vampire"
     document.getElementById("rhum2").style.transform = "scale(1)"
     document.getElementById("rorc2").style.transform = "scale(1)"
     document.getElementById("relf2").style.transform = "scale(1)"
@@ -181,44 +165,28 @@ document.getElementById("rvamp2").addEventListener("click", () => {
 });
 
 document.getElementById("bootsp2").addEventListener("click", () => {
-    itemp2.dodgeChance = 0.3
-    itemp2.bonusHeal = 0
-    itemp2.bonusDamage = 0
-    itemp2.attackTwiceChance = 0
-    itemChosenp2 = "boots";
+    p2[0].item = "boots";
     document.getElementById("bootsp2").style.transform = "scale(1.5)"
     document.getElementById("staffp2").style.transform = "scale(1)"
     document.getElementById("swordp2").style.transform = "scale(1)"
     document.getElementById("bowp2").style.transform = "scale(1)"
 });
 document.getElementById("staffp2").addEventListener("click", () => {
-    itemp2.dodgeChance = 0
-    itemp2.bonusHeal = 0.2
-    itemp2.bonusDamage = 0
-    itemp2.attackTwiceChance = 0
-    itemChosenp2 = "Staff";
+    p2[0].item = "staff";
     document.getElementById("bootsp2").style.transform = "scale(1)"
     document.getElementById("staffp2").style.transform = "scale(1.5)"
     document.getElementById("swordp2").style.transform = "scale(1)"
     document.getElementById("bowp2").style.transform = "scale(1)"
 });
 document.getElementById("swordp2").addEventListener("click", () => {
-    itemp2.dodgeChance = 0
-    itemp2.bonusHeal = 0
-    itemp2.bonusDamage = 0.3
-    itemp2.attackTwiceChance = 0
-    itemChosenp2 = "sword";
+    p2[0].item = "sword";
     document.getElementById("bootsp2").style.transform = "scale(1)"
     document.getElementById("staffp2").style.transform = "scale(1)"
     document.getElementById("swordp2").style.transform = "scale(1.5)"
     document.getElementById("bowp2").style.transform = "scale(1)"
 });
 document.getElementById("bowp2").addEventListener("click", () => {
-    itemp2.dodgeChance = 0
-    itemp2.bonusHeal = 0
-    itemp2.bonusDamage = 0
-    itemp2.attackTwiceChance = 0.3
-    itemChosenp2 = "bow";
+    p2[0].item = "bow";
     document.getElementById("bootsp2").style.transform = "scale(1)"
     document.getElementById("staffp2").style.transform = "scale(1)"
     document.getElementById("swordp2").style.transform = "scale(1)"
@@ -226,34 +194,50 @@ document.getElementById("bowp2").addEventListener("click", () => {
 });
 
 // fin création P2
+function item(player){
+    switch(player.item){
+        case "boots":
+            player.dodgeChance = 0.3;
+            break;
+        case "staff":
+            player.bonusHeal = 0.2;
+            break;
+        case "sword":
+            player.bonusDamage = 0.3;
+            break;
+        case "bow":
+            player.attackTwiceChance = 0.3;
+            break;
+    }
+}
 
 function playerProfil(){
-    switch (raceChosenp1) {
-        case "Humain":
+    switch (p1[0].race) {
+        case "human":
             document.getElementById("imgpp1").src = "../images/humanp.png";
             break;
-        case "Orc":
+        case "orc":
             document.getElementById("imgpp1").src = "../images/orcp.png";
             break;
-        case "Elf":
+        case "elf":
             document.getElementById("imgpp1").src = "../images/elfp.png";
             break;
-        case "Vampire":
+        case "vampire":
             document.getElementById("imgpp1").src = "../images/vampp.png";
             break;
         default :
 }
-    switch (raceChosenp2) {
-        case "Humain":
+    switch (p2[0].race) {
+        case "human":
             document.getElementById("imgpp2").src = "../images/humanp.png";
             break;
-        case "Orc":
+        case "orc":
             document.getElementById("imgpp2").src = "../images/orcp.png";
             break;
-        case "Elf":
+        case "elf":
             document.getElementById("imgpp2").src = "../images/elfp.png";
             break;
-        case "Vampire":
+        case "vampire":
             document.getElementById("imgpp2").src = "../images/vampp.png";
             break;
         default :
@@ -261,29 +245,25 @@ function playerProfil(){
 }
 }
 
+function textLog(text){
+    return document.getElementById("textlog").innerHTML += text + "<br>";
+}
 document.getElementById("start").addEventListener("click", () => {
     p1[0].name = (document.getElementById("p1name").value);
-    p1.push(itemp1);
     p2[0].name = (document.getElementById("p2name").value);
-    p2.push(itemp2)
     console.log(p1);
     console.log(p2);
-    console.log(`Player 1 name is ${p1[0].name} and he is playing a ${raceChosenp1}. He choose the item : ${itemChosenp1}`);
-    console.log(`Player 2 name is ${p2[0].name} and he is playing a ${raceChosenp2}. He choose the item : ${itemChosenp2}`);
+    console.log(`Player 1 name is ${p1[0].name} and he is playing the race of ${p1[0].race} equiped with : ${p1[0].item}`);
+    console.log(`Player 2 name is ${p2[0].name} and he is playing the race of ${p2[0].race}.equiped with : ${p2[0].item}`);
+    // document.getElementById("textlog").innerHTML += `Player 1 name is ${p1[0].name} and he is playing the race of ${p1[0].race} equiped with : ${p1[0].item}. <br>`
+    // document.getElementById("textlog").innerHTML += `Player 2 name is ${p2[0].name} and he is playing the race of ${p2[0].race}.equiped with : ${p2[0].item}. <br>`
     document.getElementById("cp1name").innerHTML = (p1[0].name);
     document.getElementById("cp2name").innerHTML = (p2[0].name);
     document.getElementById("fightscreen").style.display = "none";
     document.getElementById("combatlog").style.display = "block";
+    textLog(`Player 1 name is ${p1[0].name} and he is playing the race of ${p1[0].race} equiped with : ${p1[0].item}.`);
+    textLog(`Player 2 name is ${p2[0].name} and he is playing the race of ${p2[0].race}.equiped with : ${p2[0].item}.`)
     playerProfil()
+    item(p1[0]);
+    item(p2[0]);
 });
-
-// damage = 0;
-// p1[0] = Joueur , p1[1] = Bonus
-// itemChoosenp1 ou itemChoosenp2 = Item choisis  
-// let damage = 0;
-
-// function combatLog(){
-//     switch{
-//         case
-//     }
-// }
--- assets/js/test2.js
+++ assets/js/test2.js
@@ -1,5 +1,88 @@
-function perimrec(long, larg){
-    return (long + larg) *2
+// function perimrec(long, larg){
+//     return (long + larg) *2
+// }
+
+// console.log(perimrec(10, 5));
+
+
+let p1 = [
+    {
+name: "fab",
+maxHealth: 100,
+startHealth: 100,
+currentHealth: 100,
+maxDamage: 20,
+maxHealing: 30,
+damageTaken: 0.8,
+reflectChance: 0,
+lifeSteal: 0,
+item:"",
+dodgeChance: 0,
+bonusHeal: 0,
+bonusDamage: 0,
+attackTwiceChance: 0,
+}
+]
+
+let p2 = [
+    {
+name: "Mik",
+maxHealth: 100,
+startHealth: 100,
+currentHealth: 100,
+maxDamage: 20,
+maxHealing: 30,
+damageTaken: 0.8,
+reflectChance: 0,
+lifeSteal: 0,
+item:"",
+dodgeChance: 0,
+bonusHeal: 0,
+bonusDamage: 0,
+attackTwiceChance: 0,
+}
+]
+
+let itemChosenp1 = "sword";
+
+function attack(attacker, defender){
+
+    if(attacker.lifeSteal > 0){
+        defender.currentHealth -= Math.floor(defender.currentHealth/10)
+    }
+    let damage = getRandomAttack(0,20);
+
+    function getRandomAttack(min, max){
+        min = 1;
+        max = attacker.maxDamage;
+        //Retourne un entier entre min (inclus) et max (inclus)...
+        return Math.floor(Math.random() * (max - min + 1) ) + min;
+    }
+    
+    switch (attacker.item){
+        case "boots" :
+    
+    }
+
+
+
+    defender.currentHealth -= damage;
+
+    return defender.currentHealth;
+
 }
 
-console.log(perimrec(10, 5));
+function heal(){
+    return Math.floor(Math.random() * 31);
+}
+
+let dice = Math.floor(Math.random() * 2);
+let turn = 0
+
+console.log(dice)
+
+function firstTurn(){
+    Math.floor(Math.random() * 2);
+}
+
+console.log(attack(p2[0],p1[0]))
